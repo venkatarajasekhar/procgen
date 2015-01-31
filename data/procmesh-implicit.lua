1df18d66635a823db1f82af4e2477a617f960d28
Richard Fabian <raspo@humble.(none)>
2015-01-31 15:58:56 +0000

    fixed the issues. Paul's implementation had a different corner order.

--- data/procmesh-implicit.lua
+++ data/procmesh-implicit.lua
@@ -16,7 +16,7 @@ function GetMeshNormal( vert ) return table.unpack(normals[vert+1]) end
 function GetMeshUV( vert ) return table.unpack(uvs[vert+1]) end
 
 function SphereFunc( x, y, z )
-	radius = 0.5
+	radius = 7.0
 	return radius * radius - ( x * x + y * y + z * z )
 end
 function BlobFunc( x, y, z )
@@ -149,10 +149,10 @@ function ImplicitByMarchingCube( func, r )
 	
 				tris,numtris = MCLookup2(xyz)
 				if numtris  > 0 then
-					Log("Make "..numtris.." triangle")
-					for k,v in pairs(tris) do
-						Log( "K:"..k.." V:"..v )
-					end
+					--Log("From "..xyz.." Make "..numtris.." triangle")
+					--for k,v in pairs(tris) do
+						--Log( "K:"..k.." V:"..v )
+					--end
 				
 					origin = solid[index+0+0*r2+0*r22] * 1
 					xpos = solid[index+1+0*r2+0*r22] * 1
@@ -160,36 +160,36 @@ function ImplicitByMarchingCube( func, r )
 					zpos = solid[index+0+0*r2+1*r22] * 1
 					n = { origin - xpos, origin-ypos, origin-zpos }
 					
-					mxyz={x+(xmid[index]or 0.5),y+0,z+0}
-					mxYz={x+(xmid[index+r2]or 0.5),y+D,z+0}
-					mxyZ={x+(xmid[index+r22]or 0.5),y+0,z+D}
+					mxyz={x+(xmid[index]or 0),y+0,z+0}
+					mxYz={x+(xmid[index+r2]or 0),y+D,z+0}
+					mxyZ={x+(xmid[index+r22]or 0),y+0,z+D}
 					mxYZ={x+(xmid[index+r2+r22]or 0),y+D,z+D}
-					myxz={x+0,y+(ymid[index]or 0.5),z+0}
-					myXz={x+D,y+(ymid[index+1]or 0.5),z+0}
-					myxZ={x+0,y+(ymid[index+r22]or 0.5),z+D}
-					myXZ={x+D,y+(ymid[index+1+r22]or 0.5),z+D}
-					mzxy={x+0,y+0,z+(zmid[index]or 0.5)}
-					mzXy={x+D,y+0,z+(zmid[index+1]or 0.5)}
-					mzxY={x+0,y+D,z+(zmid[index+r2]or 0.5)}
-					mzXY={x+D,y+D,z+(zmid[index+1+r2]or 0.5)}
+					myxz={x+0,y+(ymid[index]or 0),z+0}
+					myXz={x+D,y+(ymid[index+1]or 0),z+0}
+					myxZ={x+0,y+(ymid[index+r22]or 0),z+D}
+					myXZ={x+D,y+(ymid[index+1+r22]or 0),z+D}
+					mzxy={x+0,y+0,z+(zmid[index]or 0)}
+					mzXy={x+D,y+0,z+(zmid[index+1]or 0)}
+					mzxY={x+0,y+D,z+(zmid[index+r2]or 0)}
+					mzXY={x+D,y+D,z+(zmid[index+1+r2]or 0)}
 
 					vertlist = {
-						mxyz,myXz,mxYz,mxyz,
-						mxyZ,myXZ,mxYZ,mxyZ,
-						mzxy,mzXy,mzXY,mzxY
+						mxyz,myXz,mxYz,myxz, -- 0,1,2,3
+						mxyZ,myXZ,mxYZ,myxZ, -- 4,5,6,7
+						mzxy,mzXy,mzXY,mzxY  -- 8,9,10,11
 					}
 
 					for t=1,numtris do
-						v1 = tris[t+0]
-						v2 = tris[t+1]
-						v3 = tris[t+2]
-						Log("Verts "..v1.." "..v2.." "..v3)
-						v1 = vertlist[1+v1]
-						v2 = vertlist[1+v2]
-						v3 = vertlist[1+v3]
-						Log("V1 "..v1[1].." "..v1[2].." "..v1[3])
-						Log("V2 "..v2[1].." "..v2[2].." "..v2[3])
-						Log("V3 "..v3[1].." "..v3[2].." "..v3[3])
+						v1 = tris[t*3-2]
+						v2 = tris[t*3-1]
+						v3 = tris[t*3-0]
+						--Log("Verts "..v1.." "..v2.." "..v3)
+						v1 = vertlist[v1+1]
+						v2 = vertlist[v2+1]
+						v3 = vertlist[v3+1]
+						--Log("V1 "..v1[1]-x.." "..v1[2]-y.." "..v1[3]-z)
+						--Log("V2 "..v2[1]-x.." "..v2[2]-y.." "..v2[3]-z)
+						--Log("V3 "..v3[1]-x.." "..v3[2]-y.." "..v3[3]-z)
 						AddTri( v1,v2,v3, n, uv1, uv2, uv3 )
 					end
 				end
@@ -203,8 +203,8 @@ function Fixup()
 	normals = {}
 	uvs = {}
 
-	ImplicitByMarchingCube( SphereFunc, 4 )
-	--ImplicitByMarchingCube( BlobFunc, 4 )
+	ImplicitByMarchingCube( SphereFunc, 7 )
+	--ImplicitByMarchingCube( BlobFunc, 7 )
 	--ImplicitByBlocks( BlobFunc, 4 )
 
 	Log("FIXUP VCount = "..vertCount)
